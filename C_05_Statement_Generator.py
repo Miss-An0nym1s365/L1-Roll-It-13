c80f166b8bc9d926fb419a8456621977caac4dc0
Miss-An0nym1s365 <jimmyc24942@masseyhigh.school.nz>
2026-02-16 13:29:17 +1300

    Working looping component(basic)

---
+++ C_05_Statement_Generator.py
@@ -0,0 +1,7 @@
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of headings"""
+
+    ends = decoration * 3
+    print(f"{ends} {statement} {ends}")
+
+
